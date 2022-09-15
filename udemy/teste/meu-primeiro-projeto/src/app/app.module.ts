b5527bc7b296d8ca5bc3cd0a3829690738784bdf
MariaClaudia1982 <mariaclaudiapio@gmail.com>
2022-09-14 21:41:58 -0300

    códigos do dia

--- udemy/teste/meu-primeiro-projeto/src/app/app.module.ts
+++ udemy/teste/meu-primeiro-projeto/src/app/app.module.ts
@@ -3,22 +3,28 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
+import { SharedModule } from './shared/shared.module';
 
 import { TitleComponent } from './title/title.component';
 import { DataBidingComponent } from './data-biding/data-biding.component';
+import { DiretivasEstruturaisComponent } from './diretivas-estruturais/diretivas-estruturais.component';
+import { DiretivasAtributosComponent } from './diretivas-atributos/diretivas-atributos.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     TitleComponent,
-    DataBidingComponent
+    DataBidingComponent,
+    DiretivasEstruturaisComponent,
+    DiretivasAtributosComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule
+    FormsModule,
+    SharedModule
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [], // utilizado para fazer requisições ao backend
+  bootstrap: [AppComponent] // só deve ser utilizado no componente principal
 })
 export class AppModule { }
