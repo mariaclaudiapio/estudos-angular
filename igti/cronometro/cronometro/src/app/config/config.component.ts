9c805b09835b05ecd8a77a3ac7acb61233be004c
MariaClaudia1982 <mariaclaudiapio@gmail.com>
2022-09-19 21:33:55 -0300

    incluindo funções de adicionar e excluir exercícios na interface config

--- igti/cronometro/cronometro/src/app/config/config.component.ts
+++ igti/cronometro/cronometro/src/app/config/config.component.ts
@@ -1,15 +1,32 @@
-import { Component, OnInit } from '@angular/core';
+import { Exercise } from './../exercise';
+import { Component } from '@angular/core';
+
 
 @Component({
   selector: 'app-config',
   templateUrl: './config.component.html',
   styleUrls: ['./config.component.css']
 })
-export class ConfigComponent implements OnInit {
+export class ConfigComponent {
+
+  exercises: Exercise[] = []
+
+  exercise: Exercise = {
+    name: '',
+    duration: 30,
+    repetitions: 3,
+    preparation: 15,
+    rest: 30
+  }
 
   constructor() { }
 
-  ngOnInit(): void {
+  add() {
+    this.exercises.push(this.exercise)
+    this.exercise = {...this.exercise, name: '' }
   }
 
+  delete(i: number) {
+    this.exercises.splice(i, 1)
+  }
 }
